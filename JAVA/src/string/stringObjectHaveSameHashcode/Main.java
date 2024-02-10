66f1bcd873d49349be7cc07df6d0d427ae6c9435
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2024-02-11 02:41:27 +0530

    string class object use string to generate hashcode
    string class object use string to generate hashcode so they have same hashcode if string is same

---
+++ JAVA/src/string/stringObjectHaveSameHashcode/Main.java
@@ -0,0 +1,13 @@
+package string.stringObjectHaveSameHashcode;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1=new String("yuv");
+		String str2=new String("yuv");
+		System.out.println(str1.hashCode());
+		System.out.println(str2.hashCode());
+	}
+
+}
