586490557beb9210cad65b3242fd06dbf9c7da51
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2024-08-16 12:21:18 +0530

    Update BankAccount.java

--- JAVA/src/multiThreading/reentrantLock/BankAccount.java
+++ JAVA/src/multiThreading/reentrantLock/BankAccount.java
@@ -6,37 +6,36 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class BankAccount {
 
-	private int balance=100;
-	
-	private final Lock lock= new ReentrantLock();
-	
-	public void withdraw(int amount)
-	{
+	private int balance = 100;
+
+	private final Lock lock = new ReentrantLock();
+
+	public void withdraw(int amount) {
+		boolean lockAcquired = false;
 		try {
-			if(lock.tryLock(1000, TimeUnit.MILLISECONDS))
-			{
-				//withdraw amount 
-				System.out.println(Thread.currentThread().getName()+" withdrawling");
-				if(balance>=amount)
-				{
-					balance-=amount;
+			lockAcquired = lock.tryLock(1000, TimeUnit.MILLISECONDS);
+			if (lockAcquired) {
+				// withdraw amount
+				System.out.println(Thread.currentThread().getName() + " withdrawling");
+				if (balance >= amount) {
+					balance -= amount;
+				} else {
+					System.out.println(Thread.currentThread().getName() + " insufficient balance");
 				}
-				else
-				{
-					System.out.println(Thread.currentThread().getName()+" insufficient balance");
-				}
-				System.out.println(Thread.currentThread().getName()+" withdrawl completed");
+				System.out.println(Thread.currentThread().getName() + " withdrawl completed");
 				System.out.println("cuurent balance is " + balance);
-				lock.unlock();
+			} else {
+				System.out.println(Thread.currentThread().getName() + " could not acquire lock");
 			}
-			else
-			{
-				System.out.println(Thread.currentThread().getName()+" could not acquire lock");
+		} catch (InterruptedException e) {
+			// log the exception
+			System.out.println("exception occured " + e.getMessage());
+			Thread.currentThread().interrupt();
+		} finally {
+			if (lockAcquired) {
+				lock.unlock(); // Ensure that we only unlock if the lock was actually acquired
 			}
-		} catch (Exception e) {
-			//log the exception
-			System.out.println("exceptio occured "+e);
 		}
 	}
-	
+
 }
